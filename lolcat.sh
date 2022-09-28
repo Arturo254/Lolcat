6f6423bfdc0902b5438feed255207eadccbe00a9
Arturo254 <87346871+Arturo254@users.noreply.github.com>
2022-09-28 16:10:41 -0500

    Add files via upload

---
+++ lolcat.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+clear
+
+sleep 2
+
+figlet -f future INSTALANDO LOLCAT
+
+sleep 2
+
+apt-get install ruby
+yum install ruby
+dnf install ruby
+
+clear
+
+sleep 3
+
+wget https://github.com/busyloop/lolcat/archive/master.zip
+
+unzip master.zip
+
+cd lolcat-master/bin
+
+gen install lolcat 
+
+rm -r lolcat-master.zip
+
+
+clear
+
+
+figlet -f future   Listo | lolcat 
+
+sleep 3
+
+cd
+
+clear && bash 
